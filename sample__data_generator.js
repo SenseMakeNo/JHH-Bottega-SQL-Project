161ccfa0bd7c2e3a4b338bb741eb80254434301c
SenseMakeNo <jhhillam@gmail.com>
2020-07-21 12:26:53 -0600

    populated sample data.

--- sample__data_generator.js
+++ sample__data_generator.js
@@ -1,3 +1,6 @@
+// I decided it would be more effiecient to hard code the
+// courses_professors_id, so the data generator
+// doesn't have an option for adding them."
 const sampleDataGenerator = (table, column, amount) => {
     let i = 1;
     const sampleDataName = table.substr(0, (table.length - 1));
@@ -8,4 +11,8 @@ const sampleDataGenerator = (table, column, amount) => {
 
 };
 
-//node sample__data_generator.js
+sampleDataGenerator('students', 'name', 99);
+sampleDataGenerator('professors', 'name', 11);
+sampleDataGenerator('courses', 'subject', 10);
+
+//RUN THE GENRATOR: node sample__data_generator.js
