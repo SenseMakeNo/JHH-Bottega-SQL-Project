853b459624f8f0686f27005f9d94a656a1305dbc
SenseMakeNo <jhhillam@gmail.com>
2020-07-21 11:22:47 -0600

    completed sampleUserGenerator

--- sample__data_generators.js
+++ sample__data_generators.js
@@ -3,11 +3,11 @@ const sampleUserGenerator = (table, amount) => {
     const sampleDataName = table.substr(0, (table.length - 1));
     
     for (i; i <= amount; i++) {
-        console.log(`INSERT_INTO ${table} (${table}_name) VALUES(${sampleDataName}${i})`)
+        console.log(`INSERT INTO ${table} (${table}_name) VALUES("${sampleDataName}${i}");`)
     }
 
 }
 
-sampleUserGenerator('tests', 3);
+sampleUserGenerator('students', 99);
 
 //node sample__data_generators.js
