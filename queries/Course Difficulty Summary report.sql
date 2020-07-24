c39df5a951acea44c40c6bc2951785099a8ac4e7
SenseMakeNo <jhhillam@gmail.com>
2020-07-24 11:56:59 -0600

    Pushed up correct version of summary report generator.

--- queries/Course Difficulty Summary report.sql
+++ queries/Course Difficulty Summary report.sql
@@ -1,6 +1,11 @@
 USE tetrarch_universty_database;
 
-SELECT c.course_subject AS Course, ROUND(AVG(g.grades_score), 2) AS 'Average grade'
+SELECT c.courses_subject AS Course, g.average AS 'Average grade'
 FROM courses c
-JOIN grades g
-ON g.grades_courses_id = c.courses_id;
+JOIN (
+	SELECT ROUND(AVG(grades_score), 2) AS average, grades_courses_id
+    FROM grades
+    GROUP BY grades_courses_id
+) g
+ON g.grades_courses_id = c.courses_id
+ORDER BY g.average ASC;
